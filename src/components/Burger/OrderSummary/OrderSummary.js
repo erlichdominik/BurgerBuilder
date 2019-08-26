c3225ea4de2b446539de8f373d297f15478086d1
Dominik Erlich <mimikdesu@gmail.com>
2019-08-26 16:56:14 +0200

    main part of app done

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Button from '../../UI/Button/Button';
+
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
         return (<li key={igKey}>
@@ -13,6 +15,8 @@ const orderSummary = (props) => {
             {ingredientSummary}
         </ul>
         <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+        <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
     </>
     );
 }
